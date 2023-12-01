5212030faf97ca75ce717ed367d5148cd2531d14
DMortal1 <cryptosodium@gmail.com>
2023-12-01 15:39:05 +0530

    display push pop clear
    can push numbers to display and pop numbers from display with number keys and backspace key, as well as clear display with clear key

--- index-script.js
+++ index-script.js
@@ -1,9 +1,29 @@
 
+// Global Variables
+let displayString = "";
+let entry1 = 0;
+let entry2 = 0;
+let currentOperation = "";
+let numberDictionary = {
+    "decimal-seperator": ".",
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+}
+
 // Declare Buttons and Display
 
 // maximum 13 digits (string length 13)
 // use Ne+n or Ne-n for values beyond these
 const displaySection = document.querySelector("#display");
+const buttonsSection = document.querySelector("#buttons");
 
 const remainderButton = document.querySelector("#remainder");
 const clearentryButton = document.querySelector("#clear-entry");
@@ -37,9 +57,37 @@ const equalsButton = document.querySelector("#equals");
 
 // Add Event Listeners
 
+buttonsSection.addEventListener('click',pushNumber);
+
+clearButton.addEventListener('click',clear);
+backspaceButton.addEventListener('click',popDisplay);
+
 // Functions
 
 function updateDisplay() {
+    displaySection.innerHTML = displayString;
+}
+
+function pushDisplay(string) {
+    if(displayString.length < 13){
+        displayString = displayString + string;
+    }
+    updateDisplay();
+}
+
+function popDisplay() {
+    if(displayString.length > 0){
+        displayString = displayString.slice(0,-1);
+    }
+    updateDisplay();
+}
+
+function pushNumber(event) {
+    let targetID = (event.target).id;
+    let toPush = numberDictionary[targetID];
+    if(toPush !== undefined) {
+        pushDisplay(toPush);
+    }
 }
 
 // Update Display Functions
@@ -51,6 +99,8 @@ function clearentry() {
 }
 
 function clear() {
+    displayString = "";
+    updateDisplay();
 }
 
 function backspace() {
