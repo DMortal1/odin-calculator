175137b55fb95138c779ae6dd7f76cf16b05a593
DMortal1 <cryptosodium@gmail.com>
2023-12-01 13:38:17 +0530

    Update display and buttons
    added the calculator display and buttons to html, css and js

---
+++ index-script.js
@@ -0,0 +1,38 @@
+
+// Declare Buttons and Display
+
+// maximum 13 digits (string length 13)
+// use Ne+n or Ne-n for values beyond these
+const displaySection = document.querySelector("#display");
+
+const remainderButton = document.querySelector("#remainder");
+const clearentryButton = document.querySelector("#clear-entry");
+const clearButton = document.querySelector("#clear");
+const backspaceButton = document.querySelector("#backspace");
+
+const reciprocalButton = document.querySelector("#reciprocal");
+const exponentButton = document.querySelector("#exponent");
+const rootButton = document.querySelector("#root");
+const divideButton = document.querySelector("#divide");
+
+const nineButton = document.querySelector("#nine");
+const eightButton = document.querySelector("#eight");
+const sevenButton = document.querySelector("#seven");
+const multiplyButton = document.querySelector("#multiply");
+
+const sixButton = document.querySelector("#six");
+const fiveButton = document.querySelector("#five");
+const fourButton = document.querySelector("#four");
+const minusButton = document.querySelector("#minus");
+
+const threeButton = document.querySelector("#three");
+const twoButton = document.querySelector("#two");
+const oneButton = document.querySelector("#one");
+const plusButton = document.querySelector("#plus");
+
+const plusminusButton = document.querySelector("#change-sign");
+const zeroButton = document.querySelector("#zero");
+const dotButton = document.querySelector("#decimal-seperator");
+const equalsButton = document.querySelector("#equals");
+
+// Add Event Listeners
